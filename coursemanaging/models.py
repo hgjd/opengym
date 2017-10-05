106b1d7f1e1ea42b5f2324b78bffdf7042484c6f
Mostaard <vandermostenjonas@gmail.com>
2017-10-05 17:25:10 +0200

    add first idea model

--- coursemanaging/models.py
+++ coursemanaging/models.py
@@ -1,3 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    BEGINNER = 1
+    INTERMEDIATE = 2
+    ADVANCED = 3
+
+    LEVEL_CHOICES = ((BEGINNER, 'Beginner'),
+                     (INTERMEDIATE, 'Intermediate'),
+                     (ADVANCED, 'Advanced'),
+                     )
+
+    course_name = models.CharField(max_length=100, null=False, blank=False)
+    course_level = models.SmallIntegerField(null=False, blank=False, choices=LEVEL_CHOICES)
+    teachers = models.ManyToManyField(User, related_name='courses_teacher')
+    students = models.ManyToManyField(User, related_name='courses_student')
+    single_session_possible = models.BooleanField(null=False, blank=False)
+    description = models.TextField()
+
+
+class Session(models.model):
+    subscribed_users = models.ForeignKey(User, related_name='Sessions')
+    start_datetime = models.DateTimeField(null=False, blank=False)
+    duration = models.DurationField(null=False, blank=False)
+    extra_info = models.TextField()
