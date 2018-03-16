2b346af9282bc04dd28847f5bba59382eadf0a1d
Mostaard <vandermostenjonas@gmail.com>
2018-03-16 11:51:27 +0100

    add possibility to change password

--- opengym/urls.py
+++ opengym/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from . import views
+from django.contrib.auth import views as auth_views
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -24,6 +24,11 @@ app_name = "opengym"
 urlpatterns = [
     url(r'^', include('coursemanaging.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
 
 ]
 
